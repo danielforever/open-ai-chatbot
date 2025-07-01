e723a2146332172b4a3aebf6a5d2acbb0d7a7ae5
danielforever <daniel87011354@gmail.com>
2025-07-01 00:40:25 -0400

    fix cosmosDB error

--- backend/cosmos.js
+++ backend/cosmos.js
@@ -1,4 +1,5 @@
 // backend/cosmos.js
+require("dotenv").config(); // Load .env file
 const { CosmosClient } = require("@azure/cosmos");
 
 const client = new CosmosClient(process.env.COSMOS_CONNECTION_STRING);
